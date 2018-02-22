b03dbea89f57983a4f0693c269d743a119855aa7
Claire Young <claire.young@timeinc.com>
2018-02-22 14:40:34 -0800

    remove the `react-native-fetch-blob` dependency (which makes rest of code unusable, but that's ok)

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ import {
     ViewPropTypes
 } from 'react-native';
 
-import RNFetchBlob from 'react-native-fetch-blob';
-
 const SHA1 = require('crypto-js/sha1');
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 import PdfView from './PdfView';
@@ -113,172 +111,14 @@ export default class Pdf extends Component {
 
     _loadFromSource = (newSource) => {
 
-        const source = resolveAssetSource(newSource) || {};
-        //__DEV__ && console.log("PDF source:");
-        //__DEV__ && console.log(source);
-
-        let uri = source.uri || '';
-
-        // no chanage then return
-        if (this.uri === uri) return;
-        this.uri = uri;
-
-        // first set to initial state
-        this.setState({ isDownloaded: false, path: '', progress: 0 });
-
-        const tempCacheFile = RNFetchBlob.fs.dirs.CacheDir + '/' + SHA1(uri) + '.pdf.tmp';
-        const cacheFile = RNFetchBlob.fs.dirs.CacheDir + '/' + SHA1(uri) + '.pdf';
-
-        if (source.cache) {
-            RNFetchBlob.fs
-                .exists(tempCacheFile)
-                .then(exist => {
-                    if (exist) {
-                        // delete temp file
-                        RNFetchBlob.fs.unlink(tempCacheFile);
-                        // download from source
-                        this._prepareFile(source)
-                    } else {
-                        RNFetchBlob.fs
-                            .exists(cacheFile)
-                            .then(exist => {
-                                if (exist) {
-                                    this.setState({ path: cacheFile, isDownloaded: true })
-                                } else {
-                                    // cache not exist then re load it
-                                    this._prepareFile(source)
-                                }
-                            })
-                            .catch(() => {
-                                this._prepareFile(source)
-                            })
-                    }
-                })
-                .catch(() => {
-                    this._prepareFile(source)
-                })
-        } else {
-            this._prepareFile(source)
-        }
 
     };
 
     _prepareFile = (source) => {
 
-        if (source.uri) {
-            let uri = source.uri || '';
-
-            const isNetwork = !!(uri && uri.match(/^https?:\/\//));
-            const isAsset = !!(uri && uri.match(/^bundle-assets:\/\//));
-            const isBase64 = !!(uri && uri.match(/^data:application\/pdf;base64/));
-
-            const tempCacheFile = RNFetchBlob.fs.dirs.CacheDir + '/' + SHA1(uri) + '.pdf.tmp';
-            const cacheFile = RNFetchBlob.fs.dirs.CacheDir + '/' + SHA1(uri) + '.pdf';
-
-            // delete old cache file
-            RNFetchBlob.fs.unlink(tempCacheFile);
-            RNFetchBlob.fs.unlink(cacheFile);
-
-            if (isNetwork) {
-                this._downloadFile(source, tempCacheFile, cacheFile)
-            } else if (isAsset) {
-                RNFetchBlob.fs
-                    .cp(uri, cacheFile)
-                    .then(() => {
-                        //__DEV__ && console.log("load from asset:"+uri);
-                        this.setState({ path: cacheFile, isDownloaded: true })
-                    })
-                    .catch(error => {
-                        RNFetchBlob.fs.unlink(cacheFile);
-                        console.warn('load from asset error');
-                        console.log(error);
-                        this.props.onError && this.props.onError('load pdf failed.')
-                    })
-            } else if (isBase64) {
-                let data = uri.replace(/data:application\/pdf;base64,/i, '');
-                RNFetchBlob.fs
-                    .writeFile(cacheFile, data, 'base64')
-                    .then(() => {
-                        //__DEV__ && console.log("write base64 to file:" + cacheFile);
-                        this.setState({ path: cacheFile, isDownloaded: true })
-                    })
-                    .catch(() => {
-                        RNFetchBlob.fs.unlink(this.path);
-                        console.warn('write base64 file error!');
-                        this.props.onError && this.props.onError('load pdf failed.')
-                    })
-            } else {
-                //__DEV__ && console.log("default source type as file");
-                this.setState({
-                    path: uri.replace(/file:\/\//i, ''),
-                    isDownloaded: true,
-                })
-            }
-        } else {
-            console.error('no pdf source!');
-        }
-
     };
 
     _downloadFile = (source, tempCacheFile, cacheFile) => {
-
-        if (this.lastRNBFTask) {
-            this.lastRNBFTask.cancel(err => {
-                RNFetchBlob.fs.unlink(tempCacheFile);
-                //__DEV__ && console.log("Load pdf from url was cancelled.");
-            });
-            this.lastRNBFTask = null;
-        }
-
-        this.lastRNBFTask = RNFetchBlob.config({
-            // response data will be saved to this path if it has access right.
-            path: tempCacheFile,
-        })
-            .fetch(
-            source.method ? source.method : 'GET',
-            source.uri,
-            source.headers ? source.headers : {}
-            )
-            // listen to download progress event
-            .progress((received, total) => {
-                //__DEV__ && console.log('progress', received / total);
-                this.props.onLoadProgress && this.props.onLoadProgress(received / total);
-                this.setState({ progress: received / total })
-            });
-
-        this.lastRNBFTask
-            .then(res => {
-                let { status } = res.respInfo;
-
-                this.lastRNBFTask = null;
-
-                //__DEV__ && console.log('Load pdf from url and saved to ', res.path())
-
-                switch (status) {
-                    case 200: /* OK */
-                    case 204: /* No content */
-                    case 304: /* Not modified */
-                    {
-                        RNFetchBlob.fs
-                            .mv(tempCacheFile,cacheFile)
-                            .then(() => {
-                                //__DEV__ && console.log("load from asset:"+uri);
-                                this.setState({ path: cacheFile, isDownloaded: true, progress: 1 });
-                            });
-                        break;
-                    }
-                    default:
-                        RNFetchBlob.fs.unlink(tempCacheFile);
-                        this.props.onError && this.props.onError(`load pdf failed with code ${status}`);
-                        break;
-                }
-            })
-            .catch(error => {
-                console.warn(`download ${source.uri} error:${error}.`);
-                this.lastRNBFTask = null;
-                RNFetchBlob.fs.unlink(tempCacheFile);
-                this.props.onError && this.props.onError('load pdf failed.')
-            });
 
     };
 
@@ -290,21 +130,6 @@ export default class Pdf extends Component {
 
     _onChange = (event) => {
 
-        let message = event.nativeEvent.message.split('|');
-        //__DEV__ && console.log("onChange: " + message);
-        if (message.length > 0) {
-            if (message[0] === 'loadComplete') {
-                this.props.onLoadComplete && this.props.onLoadComplete(Number(message[1]), this.state.path);
-            } else if (message[0] === 'pageChanged') {
-                this.props.onPageChanged && this.props.onPageChanged(Number(message[1]), Number(message[2]));
-            } else if (message[0] === 'error') {
-                this._onError(message[1]);
-            } else if (message[0] === 'pageSingleTap') {
-                this.props.onPageSingleTap && this.props.onPageSingleTap(message[1]);
-            } else if (message[0] === 'scaleChanged') {
-                this.props.onScaleChanged && this.props.onScaleChanged(message[1]);
-            }
-        }
 
     };
     _onError = (error) => {
